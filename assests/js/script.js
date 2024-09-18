071dc44c9ace2ba5c4a55e9ebdb865feee5a734c
NADAMAHer74 <nada01141089562@gmail.com>
2024-09-18 03:59:25 +0300

    editcommit5

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {
+window.onscroll = function () {
   scrollFunction();
 };
 
@@ -13,7 +13,7 @@ function scrollFunction() {
     mybutton.style.position = "fixed";
     mybutton.style.bottom = "20px";
     mybutton.style.right = "10px";
-    mybutton.style.transition= "0.3s ease-in-out"
+    mybutton.style.transition = "0.3s ease-in-out";
   } else {
     mybutton.style.display = "none";
   }
@@ -22,22 +22,39 @@ function scrollFunction() {
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+document.querySelector(".toggleChevron").addEventListener("click", function () {
+  var contactMenu = document.querySelector(".contactMenu");
 
-document.querySelector('.toggleChevron').addEventListener('click', function () {
-    var contactMenu = document.querySelector('.contactMenu');
-    
-    // Toggle the display of the contact menu
-    if (contactMenu.style.display === 'none' || contactMenu.style.display === '') {
-      contactMenu.style.display = 'flex'; // Show the list
-    } else {
-      contactMenu.style.display = 'none'; // Hide the list
-    }
-  });
+  // Toggle the display of the contact menu
+  if (
+    contactMenu.style.display === "none" ||
+    contactMenu.style.display === ""
+  ) {
+    contactMenu.style.display = "flex"; // Show the list
+  } else {
+    contactMenu.style.display = "none"; // Hide the list
+  }
+});
+
+const images = [
+  "./assests/images/slider-1-2.jpg",
+  "./assests/images/slider-1-3.jpg",
+  "./assests/images/slider-1-1.jpg",
+];
+
+let currentIndex = 0;
+
+function changeBackground() {
+  const backgroundImage = document.getElementById("dynamicBackground");
+  currentIndex = (currentIndex + 1) % images.length;
+  backgroundImage.src = images[currentIndex];
+}
+setInterval(changeBackground, 1000);
