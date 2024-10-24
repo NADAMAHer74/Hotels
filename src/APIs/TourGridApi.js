898bea83923dc21210ff63848e41866e827f5b07
2Ahmed4Omar <ahmed.w.dev01@gmail.com>
2024-10-25 02:46:39 +0300

    Edited

--- src/APIs/TourGridApi.js
+++ src/APIs/TourGridApi.js
@@ -5,6 +5,7 @@ import axios from "axios";
 const API_ENDPOINT = "https://eecf8975-6c57-4990-969b-6a32dc2c0aff.mock.pstmn.io/hotels";
 
 export const fetchTours = createAsyncThunk("tourGrid/fetchTours", async (page) => {
+    const limit = 6;
     const response = await axios.get(`${API_ENDPOINT}?page=${page}&limit=${limit}`);
     return {
         tours: response.data,
